7b2d3aac069b0776ad3f1746398296c9441f7f57
Joris <joris.lambrechts@kuleuven.be>
2017-11-22 10:12:52 +0100

    close #3

--- themes/default/common/footer.php
+++ themes/default/common/footer.php
@@ -6,32 +6,39 @@
                   <p><?php echo $copyright; ?></p>
               <?php endif; ?>
               <div class="row">
-                  <div class="col-sm-7 col-lg-6">
-                      <div class="logo-row">
-                          <a href="https://uclouvain.be"><img class="ucl" src="<?php echo img("ucl.jpg");?>"></a>
-                          <a href="https://kuleuven.be"><img src="<?php echo img("KULEUVEN.png");?>"></a>
-                          <a href="http://libis.be"><img src="<?php echo img("libis_gray.png");?>"></a>
-                      </div>
+                  <div class="col-xs-12 col-md-4">
+
+                    <div class="contact-info leuven">
+                      <b>KU Leuven Libraries Special Collections</b><br>
+                      Mgr. Ladeuzeplein 21 box 5591 |
+                      3000 Leuven<br>
+                      bijzonderecollecties@kuleuven.be<br>
+                      tel.+32 (0)16 32 46 24
+                    </div>
                   </div>
-                  <div class="col-xs-12 col-sm-3">
-                      <p>
-                          Straatstraat 22<br>
-                          3000 Leuven<br>
-                          016222222<br>
-                          .....
-                      </p>
+                  <div class="col-md-5 col-xs-12">
+                    <div class="contact-info">
+                      <b>Bibliothèques de l’Université catholique de Louvain<br>
+                      Réserve patrimoniale</b><br>
+                      Grand-Place 45 |
+                      1348 Louvain-la-Neuve<br>
+                      respat-sceb@uclouvain.be<br>
+                      tel. +32 (0)10 47 81 87
+                    </div>
                   </div>
-                  <div class="col-md-3 col-xs-12">
-                      <ul>
-                          <li><a href="">Menu item 1</a></li>
-                          <li><a href="">Menu item 1</a></li>
-                          <li><a href="">Menu item 1</a></li>
-                          <li><a href="">Menu item 1</a></li>
-                      </ul>
+                </div>
+                <div class="row logo-row">
+                  <div class="col-xs-12">
+                    <div class="logos">
+                    <a href="https://uclouvain.be/en/libraries"><img class="ucl" src="<?php echo img("ucl.jpg");?>"></a>
+                    <a href="https://bib.kuleuven.be/english/"><img src="<?php echo img("KULEUVEN.png");?>"></a>
+                    <a href="http://libis.be"><img src="<?php echo img("libis_gray.png");?>"></a>
+                    <a href="http://www.cultuurculture.be/"><img src="<?php echo img("cultuur-culture.jpg");?>"></a>
                   </div>
+                </div>
               </div>
           </div>
-          <div class="copyright">© test</div>
+          <div class="copyright">© Lovaniensia</div>
           <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
       </div>
     </footer><!-- end footer -->
