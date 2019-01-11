a17cc83bce8e753abc6fbe36406c23961b5e35b2
Joris <joris.lambrechts@kuleuven.be>
2019-01-11 10:47:38 +0100

    label change

--- themes/default/items/show.php
+++ themes/default/items/show.php
@@ -191,7 +191,7 @@
 
                 <?php if($text = metadata('item', array('Item Type Metadata','External manuscript'),array("all"=>true))):?>
                   <div class="element">
-                      <h3><?php echo __('Other digitised copies');?></h3>
+                      <h3><?php echo __('Externally digitised copies');?></h3>
                       <ul class="element-text">
                       <?php foreach($text as $manu):?>
                         <li ><?php echo $manu;?></li>
